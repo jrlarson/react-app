7cb1c8c102c4fc66514d1a5ca521bfa099f24ddd
John Larson <jlarson@veloracer.com>
2017-10-15 11:16:59 -0500

    Formatting cleanup and move node server to separate repo.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
     "env": {
         "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": "plugin:react/recommended",
     "parserOptions": {
         "ecmaFeatures": {
             "experimentalObjectRestSpread": true,
@@ -19,6 +19,25 @@ module.exports = {
             "error",
             "single"
         ],
+        "semi": [
+            "error",
+            "always"
+        ],
+        "import/prefer-default-export": [0],
+        "class-methods-use-this": [0],
+        "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+        "react/forbid-prop-types":  [1, {"forbid": []}],
+        "no-plusplus": [0],
         "no-unused-vars": ["error", { "vars": "local", "args": "after-used", "ignoreRestSiblings": false}]
-    }
+    },
+    "settings": {
+        "react": {
+          "createClass": "createReactClass", // Regex for Component Factory to use, default to "createReactClass"
+          "pragma": "React",  // Pragma to use, default to "React"
+          "version": "15.0", // React version, default to the latest React stable release
+          "flowVersion": "0.53" // Flow version
+        },
+        "propWrapperFunctions": [ "forbidExtraProps" ] // The names of any functions used to wrap the propTypes object, such as `forbidExtraProps`. If this isn't set, any propTypes wrapped in a function will be skipped.
+      }
+    
 };
